03b6dd1f335c3c5b2254941ac8fee93f84640a92
Bastian Wirth <bastian.wirth@upinblue.com>
2019-09-29 11:49:45 +0200

    Initial Commit

---
+++ UIBBusinessCardKit/UIBBusinessCardKit.h
@@ -0,0 +1,19 @@
+//
+//  UIBBusinessCardKit.h
+//  UIBBusinessCardKit
+//
+//  Created by Bastian Wirth on 29.09.19.
+//  Copyright © 2019 up in blue GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIBBusinessCardKit.
+FOUNDATION_EXPORT double UIBBusinessCardKitVersionNumber;
+
+//! Project version string for UIBBusinessCardKit.
+FOUNDATION_EXPORT const unsigned char UIBBusinessCardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIBBusinessCardKit/PublicHeader.h>
+
+
